45946b032a92108db911e54e05cd8ab7090ec6b2
Paulo Antonio <paulo.werder@gmail.com>
2025-10-15 16:14:21 -0300

    create plugin event

---
+++ plugins/atlas/events/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Atlas\Events;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
